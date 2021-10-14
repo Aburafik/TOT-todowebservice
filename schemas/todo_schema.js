384d06bfda98b0527f0977d19790dd3e0838d08c
Aburafik <rfkabubakari@gmail.com>
2021-10-14 10:09:45 -0700

    TOT Todo Web service final commit

---
+++ schemas/todo_schema.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const {Schema, model}= mongoose
+
+const todoSchema=Schema({
+    title:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true
+    },
+
+    date_time:{
+        type:String,
+        required: true
+    },
+    status:{
+        type:Boolean,
+        required: true,
+        default:false
+    }
+})
+
+// const TodoModel= mongoose.model('todo',todoSchema);
+const todoModel = model('todo', todoSchema);
+export default todoModel; 
+
+// module.exports=TodoModel;
